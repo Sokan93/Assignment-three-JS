f88a6e76c307095898b1a51c4e88d8d076a48785
Ayodeji Sokan <ayodejisokan@gmail.com>
2024-07-22 17:45:13 -0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+//ASSIGNMENT THREE - AYODEJI SOKAN
+//Create variables for the following
+//Strings,numbers,boolean,null,undefined
+
+
+let fullName = "Ayodeji Sokan"; //String 
+
+let age = 30; //Number
+
+let citizenOfNigeria = true; //Boolean
+
+let accountName = null; //Null
+
+let accountBalance; //Undefined
+
+console.log(fullName);
+console.log(age);
+console.log(citizenOfNigeria);
+console.log(accountName);
+console.log(accountBalance);
